48eafdccab5d840d8ffc01f6201fea79bb0546f1
Max Dobeck <max.dobeck@evinced.com>
2024-06-17 20:14:35 -0400

    added basic tests with enzyme, jsdom, and react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-   return <div>Hello, React 17 is running</div>;
+      return (
+         <div>
+            Hello, React 17 is running
+            <h1 id="target">Target Text</h1>
+         </div>
+      );
 };
 
 export default App;
